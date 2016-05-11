cdb8bbc1f0d6758af1d6d1809ef6cee26b5fefd4
xanthics <3137623+xanthics@users.noreply.github.com>
2016-05-10 22:52:23 -0700

    Expanded animate weapon rules for highlighting ideal pickup

--- animate_weapon.py
+++ animate_weapon.py
@@ -32,22 +32,22 @@ desc = "Animate Weapon target"
 
 # Base type : settings pair
 items = {
-	"0 animate melee white 4x1": {"other": ["Class Dagger \"One Hand\" Stave \"Two Hand\" Sceptre Claws", "Rarity Normal", "Height 4", "Width 1"], "type": "ignore"},
+	"0 animate melee white 4x1": {"other": ["Class Dagger \"One Hand\" Stave \"Two Hand\" Sceptre Claws", "Rarity Normal", "Height 4", "Width 1"], "type": "animate melee b"},
 	"0 animate range white 4x1": {"other": ["Class Wand Bow", "Rarity Normal", "Height 4", "Width 1"], "type": "ignore"},
 	"0 animate melee magic 4x1": {"other": ["Class Dagger \"One Hand\" Stave \"Two Hand\" Sceptre Claws", "Rarity Magic", "Height 4", "Width 1"], "type": "ignore"},
 	"0 animate range magic 4x1": {"other": ["Class Wand Bow", "Rarity Magic", "Height 4", "Width 1"], "type": "ignore"},
 
-	"0 animate melee white 3x1": {"other": ["Class Dagger \"One Hand\" Stave \"Two Hand\" Sceptre Claws", "Rarity Normal", "Height 3", "Width 1"], "type": "ignore"},
+	"0 animate melee white 3x1": {"other": ["Class Dagger \"One Hand\" Stave \"Two Hand\" Sceptre Claws", "Rarity Normal", "Height 3", "Width 1"], "type": "animate melee b"},
 	"0 animate range white 3x1": {"other": ["Class Wand Bow", "Rarity Normal", "Height 3", "Width 1"], "type": "ignore"},
 	"0 animate melee magic 3x1": {"other": ["Class Dagger \"One Hand\" Stave \"Two Hand\" Sceptre Claws", "Rarity Magic", "Height 3", "Width 1"], "type": "ignore"},
 	"0 animate range magic 3x1": {"other": ["Class Wand Bow", "Rarity Magic", "Height 3", "Width 1"], "type": "ignore"},
 
-	"0 animate melee white 2x2": {"other": ["Class Dagger \"One Hand\" Stave \"Two Hand\" Sceptre Claws", "Rarity Normal", "Height 2", "Width 2"], "type": "ignore"},
+	"0 animate melee white 2x2": {"other": ["Class Dagger \"One Hand\" Stave \"Two Hand\" Sceptre Claws", "Rarity Normal", "Height 2", "Width 2"], "type": "animate melee b"},
 	"0 animate range white 2x2": {"other": ["Class Wand Bow", "Rarity Normal", "Height 2", "Width 2"], "type": "ignore"},
 	"0 animate melee magic 2x2": {"other": ["Class Dagger \"One Hand\" Stave \"Two Hand\" Sceptre Claws", "Rarity Magic", "Height 2", "Width 2"], "type": "ignore"},
 	"0 animate range magic 2x2": {"other": ["Class Wand Bow", "Rarity Magic", "Height 2", "Width 2"], "type": "ignore"},
 
-	"1 animate melee white": {"other": ["Class Dagger \"One Hand\" Stave \"Two Hand\" Sceptre Claws", "Rarity Normal"], "type": "ignore"},
+	"1 animate melee white": {"other": ["Class Dagger \"One Hand\" Stave \"Two Hand\" Sceptre Claws", "Rarity Normal"], "type": "animate melee"},
 	"1 animate range white": {"other": ["Class Wand Bow", "Rarity Normal"], "type": "ignore"},
 	"1 animate melee magic": {"other": ["Class Dagger \"One Hand\" Stave \"Two Hand\" Sceptre Claws", "Rarity Magic"], "type": "ignore"},
 	"1 animate range magic": {"other": ["Class Wand Bow", "Rarity Magic"], "type": "ignore"},
