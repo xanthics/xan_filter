921d61142e631fb7064feefbc865e0f05afcc939
xanthics <3137623+xanthics@users.noreply.github.com>
2016-09-04 05:56:42 -0700

    Updated map tiers to reflect new white/yellow/red tiers

--- item_config/maps.py
+++ item_config/maps.py
@@ -259,8 +259,8 @@ items = {
 	"0 Fragment of the Chimera": {"base": "Fragment of the Chimera", "class": "Map Fragments", "type": "map red good"},
 	"0 Fragment of the Hydra": {"base": "Fragment of the Hydra", "class": "Map Fragments", "type": "map red good"},
 
-	"71 Maps >= 79": {"class": "Maps", "other": ["DropLevel >= 79"], "type": "map red"},
-	"72 Maps <= 73": {"class": "Maps", "other": ["DropLevel <= 73"], "type": "map white"},
+	"71 Maps >= 78": {"class": "Maps", "other": ["DropLevel >= 78"], "type": "map red"},
+	"72 Maps <= 72": {"class": "Maps", "other": ["DropLevel <= 72"], "type": "map white"},
 	"73 Other maps": {"class": "Maps", "type": "map yellow"},
 	"74 Map Fragments": {"class": "Map Fragments", "type": "map white"},
 }
