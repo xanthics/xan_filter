3abbec31636fee9a6d0b1135b219132c9c616847
xanthics <3137623+xanthics@users.noreply.github.com>
2016-09-04 06:15:39 -0700

    Personal Tweaks

--- item_config/rares.py
+++ item_config/rares.py
@@ -30,15 +30,15 @@ desc = "General Rares"
 items = {
     "1 Quiver": {"class": "Quivers", "type": "rare low"},
 
-    "7 5": {"other": ["DropLevel <= 5", "ItemLevel >= 15"], "type": "rare low"},
-    "7 10": {"other": ["DropLevel <= 10", "ItemLevel >= 20"], "type": "rare low"},
-    "7 15": {"other": ["DropLevel <= 15", "ItemLevel >= 25"], "type": "rare low"},
-    "7 20": {"other": ["DropLevel <= 20", "ItemLevel >= 30"], "type": "rare low"},
-    "7 25": {"other": ["DropLevel <= 25", "ItemLevel >= 35"], "type": "rare low"},
-    "7 30": {"other": ["DropLevel <= 30", "ItemLevel >= 40"], "type": "rare low"},
-    "7 35": {"other": ["DropLevel <= 35", "ItemLevel >= 45"], "type": "rare low"},
-    "7 40": {"other": ["DropLevel <= 40", "ItemLevel >= 50"], "type": "rare low"},
-    "7 45": {"other": ["DropLevel <= 45", "ItemLevel >= 55"], "type": "rare low"},
+    "7 5": {"other": ["Rarity Rare", "DropLevel <= 5", "ItemLevel >= 15"], "type": "low"},
+    "7 10": {"other": ["Rarity Rare", "DropLevel <= 10", "ItemLevel >= 20"], "type": "low"},
+    "7 15": {"other": ["Rarity Rare", "DropLevel <= 15", "ItemLevel >= 25"], "type": "low"},
+    "7 20": {"other": ["Rarity Rare", "DropLevel <= 20", "ItemLevel >= 30"], "type": "low"},
+    "7 25": {"other": ["Rarity Rare", "DropLevel <= 25", "ItemLevel >= 35"], "type": "low"},
+    "7 30": {"other": ["Rarity Rare", "DropLevel <= 30", "ItemLevel >= 40"], "type": "low"},
+    "7 35": {"other": ["Rarity Rare", "DropLevel <= 35", "ItemLevel >= 45"], "type": "low"},
+    "7 40": {"other": ["Rarity Rare", "DropLevel <= 40", "ItemLevel >= 50"], "type": "low"},
+    "7 45": {"other": ["Rarity Rare", "DropLevel <= 45", "ItemLevel >= 55"], "type": "low"},
 
-    "9 Other rares": {"type": "rare low"}
+    "9 Other rares": {"other": ["Rarity Rare"], "type": "low"}
 }
