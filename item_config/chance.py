4a6546814027e0b42e8c99dd32fdb821e246ca26
xanthics <3137623+xanthics@users.noreply.github.com>
2016-09-04 17:04:50 -0700

    Updated chance list.

--- item_config/chance.py
+++ item_config/chance.py
@@ -28,17 +28,17 @@ desc = "Chance Base"
 
 # Base type : settings pair
 items = {
-	"0 Aegis Aurora": {"base": "Champion Kite Shield", "type": "chance"},
-	"0 Heretic's Veil": {"base": "Deicide Mask", "type": "chance"},
+	"0 Aegis Aurora": {"base": "Champion Kite Shield", "type": "ignore"},
+	"0 Heretic's Veil": {"base": "Deicide Mask", "type": "ignore"},
 	"0 Taste of Hate": {"base": "Sapphire Flask", "type": "ignore"},
 	"0 Lightning Coil": {"base": "Desert Brigandine", "type": "ignore"},
-	"0 Cospri's Will": {"base": "Assassin's Garb", "type": "chance"},
+	"0 Cospri's Will": {"base": "Assassin's Garb", "type": "ignore"},
 	"0 Rathpith Globe": {"base": "Titanium Spirit Shield", "type": "ignore"},
 	"0 Carcass Jack": {"base": "Varnished Coat", "type": "ignore"},
 
-	"0 Shavronne's Wrappings": {"base": "Occultist's Vestment", "type": "chance"},
-	"0 Kaom's Heart": {"base": "Glorious Plate", "type": "chance"},
-	"0 Void Battery": {"base": "Prophecy Wand", "type": "chance"},
+	"0 Shavronne's Wrappings": {"base": "Occultist's Vestment", "type": "ignore"},
+	"0 Kaom's Heart": {"base": "Glorious Plate", "type": "ignore"},
+	"0 Void Battery": {"base": "Prophecy Wand", "type": "ignore"},
 	"0 Soul Taker": {"base": "Siege Axe", "type": "ignore"},
 	"0 Skyforth": {"base": "Sorcerer Boots", "type": "chance"},
 	"0 Headhunter": {"base": "Leather Belt", "other": ["ItemLevel >= 74"], "type": "ignore"}
