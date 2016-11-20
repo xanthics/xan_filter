8176a1354a69ee744fa89cd0cdb8842dd3713a87
xanthics <3137623+xanthics@users.noreply.github.com>
2016-11-19 16:37:51 -0800

    Added support to scrap currency.poe.trade for current market rates

---
+++ auto_gen/currencyrates.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Created: 11/20/2016(m/d/y) 00:32:49 UTC from "Standard" data
+"""
+* Copyright (c) 2016 Jeremy Parks. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+
+Author: Jeremy Parks
+Purpose: Create an item filter based on config files
+Note: Requires Python 3.4.x
+"""
+
+desc = "Currency Rates"
+
+# Base type : settings pair
+items = {
+	"alch": 0.2,
+	"alt": 0.058823529411764705,
+	"blessed": 0.14285714285714285,
+	"chance": 0.16666666666666666,
+	"chaos": 1,
+	"chisel": 0.2,
+	"chrom": 0.08333333333333333,
+	"divine": 19.0,
+	"exa": 65.5,
+	"fuse": 0.4,
+	"gcp": 1.0,
+	"jew": 0.09523809523809523,
+	"mirror": 20000,
+	"regal": 0.5,
+	"regret": 1.25,
+	"scour": 0.5882352941176471,
+	"silver": 0.1,
+	"vaal": 1.1,
+}
