43b52f25dc9a912c0daadd8f8d36eefc5e200552
xanthics <xanthic42@gmail.com>
2016-12-23 18:19:37 -0800

    Currency rates updated.  Now with 100% more correct data.

--- auto_gen/hccurrency.py
+++ auto_gen/hccurrency.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Created: 12/22/2016(m/d/y) 15:28:06 UTC from "Hardcore" data
+# Created: 12/24/2016(m/d/y) 02:14:36 UTC from "Hardcore" data
 """
 * Copyright (c) 2016 Jeremy Parks. All rights reserved.
 *
