401da5f7df8f87325425480635d77e303fc1e240
xanthics <3137623+xanthics@users.noreply.github.com>
2016-12-27 23:17:50 -0800

    Market update.

--- item_config/rares.py
+++ item_config/rares.py
@@ -28,17 +28,22 @@ desc = "General Rares"
 
 # Base type : settings pair
 items = {
-    "1 Quiver": {"class": "Quivers", "type": "rare low"},
 
-    "7 5": {"other": ["Rarity Rare", "DropLevel <= 5", "ItemLevel >= 15"], "type": "low"},
-    "7 10": {"other": ["Rarity Rare", "DropLevel <= 10", "ItemLevel >= 20"], "type": "low"},
-    "7 15": {"other": ["Rarity Rare", "DropLevel <= 15", "ItemLevel >= 25"], "type": "low"},
-    "7 20": {"other": ["Rarity Rare", "DropLevel <= 20", "ItemLevel >= 30"], "type": "low"},
-    "7 25": {"other": ["Rarity Rare", "DropLevel <= 25", "ItemLevel >= 35"], "type": "low"},
-    "7 30": {"other": ["Rarity Rare", "DropLevel <= 30", "ItemLevel >= 40"], "type": "low"},
-    "7 35": {"other": ["Rarity Rare", "DropLevel <= 35", "ItemLevel >= 45"], "type": "low"},
-    "7 40": {"other": ["Rarity Rare", "DropLevel <= 40", "ItemLevel >= 50"], "type": "low"},
-    "7 45": {"other": ["Rarity Rare", "DropLevel <= 45", "ItemLevel >= 55"], "type": "low"},
+    "1 Gloves": {"other": ["ItemLevel >= 84"], "class": "Gloves", "type": "rare low"},
+    "1 Boots": {"other": ["ItemLevel >= 84"], "class": "Boots", "type": "rare low"},
+    "1 Body Armours": {"other": ["ItemLevel >= 84"], "class": "Body Armours", "type": "ignore"},
+    "1 Helmets": {"other": ["ItemLevel >= 84"], "class": "Helmets", "type": "rare low"},
 
-    "9 Other rares": {"other": ["Rarity Rare"], "type": "low"}
+    # 'rare low' after this
+    "7 5": {"other": ["DropLevel <= 5", "ItemLevel >= 15"], "type": "ignore"},
+    "7 10": {"other": ["DropLevel <= 10", "ItemLevel >= 20"], "type": "ignore"},
+    "7 15": {"other": ["DropLevel <= 15", "ItemLevel >= 25"], "type": "ignore"},
+    "7 20": {"other": ["DropLevel <= 20", "ItemLevel >= 30"], "type": "ignore"},
+    "7 25": {"other": ["DropLevel <= 25", "ItemLevel >= 35"], "type": "ignore"},
+    "7 30": {"other": ["DropLevel <= 30", "ItemLevel >= 40"], "type": "ignore"},
+    "7 35": {"other": ["DropLevel <= 35", "ItemLevel >= 45"], "type": "ignore"},
+    "7 40": {"other": ["DropLevel <= 40", "ItemLevel >= 50"], "type": "ignore"},
+    "7 45": {"other": ["DropLevel <= 45", "ItemLevel >= 55"], "type": "ignore"},
+
+    "9 Other rares": {"type": "ignore"}
 }
