d4a0c13798fc806a6954cb25d3842e212c078b91
xanthics <3137623+xanthics@users.noreply.github.com>
2017-03-03 05:06:50 -0800

    Initial Legacy League commit

--- item_config/rares.py
+++ item_config/rares.py
@@ -35,15 +35,15 @@ items = {
     "1 Helmets": {"other": ["ItemLevel >= 84"], "class": "Helmets", "type": "rare low"},
 
     # 'rare low' after this
-    "7 5": {"other": ["DropLevel <= 5", "ItemLevel >= 15"], "type": "ignore"},
-    "7 10": {"other": ["DropLevel <= 10", "ItemLevel >= 20"], "type": "ignore"},
-    "7 15": {"other": ["DropLevel <= 15", "ItemLevel >= 25"], "type": "ignore"},
-    "7 20": {"other": ["DropLevel <= 20", "ItemLevel >= 30"], "type": "ignore"},
-    "7 25": {"other": ["DropLevel <= 25", "ItemLevel >= 35"], "type": "ignore"},
-    "7 30": {"other": ["DropLevel <= 30", "ItemLevel >= 40"], "type": "ignore"},
-    "7 35": {"other": ["DropLevel <= 35", "ItemLevel >= 45"], "type": "ignore"},
-    "7 40": {"other": ["DropLevel <= 40", "ItemLevel >= 50"], "type": "ignore"},
-    "7 45": {"other": ["DropLevel <= 45", "ItemLevel >= 55"], "type": "ignore"},
+    "7 5": {"other": ["DropLevel <= 5", "ItemLevel >= 15"], "type": "rare low"},
+    "7 10": {"other": ["DropLevel <= 10", "ItemLevel >= 20"], "type": "rare low"},
+    "7 15": {"other": ["DropLevel <= 15", "ItemLevel >= 25"], "type": "rare low"},
+    "7 20": {"other": ["DropLevel <= 20", "ItemLevel >= 30"], "type": "rare low"},
+    "7 25": {"other": ["DropLevel <= 25", "ItemLevel >= 35"], "type": "rare low"},
+    "7 30": {"other": ["DropLevel <= 30", "ItemLevel >= 40"], "type": "rare low"},
+    "7 35": {"other": ["DropLevel <= 35", "ItemLevel >= 45"], "type": "rare low"},
+    "7 40": {"other": ["DropLevel <= 40", "ItemLevel >= 50"], "type": "rare low"},
+    "7 45": {"other": ["DropLevel <= 45", "ItemLevel >= 55"], "type": "rare low"},
 
     "9 Other rares": {"type": "ignore"}
 }
