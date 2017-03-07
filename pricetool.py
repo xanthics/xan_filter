2801ef2906d2d611778045cebf2c0e7cf33c5d7b
xanthics <xanthic42@gmail.com>
2017-03-06 20:10:54 -0800

    Added License file and removed license headers from all files.

--- pricetool.py
+++ pricetool.py
@@ -1,32 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-"""
-* Copyright (c) 2016 Jeremy Parks. All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a
-* copy of this software and associated documentation files (the "Software"),
-* to deal in the Software without restriction, including without limitation
-* the rights to use, copy, modify, merge, publish, distribute, sublicense,
-* and/or sell copies of the Software, and to permit persons to whom the
-* Software is furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-* DEALINGS IN THE SOFTWARE.
-
-Author: Jeremy Parks
-Purpose: Create price lists for uniques and divination cards from ggg api data
-         This file creates updated priority lists for uniques and divination cards
-Note: Requires Python 3.4.x
-      Requires a local MongoDB 3.2.x instance
-"""
+# Author: Jeremy Parks
+# Note: Requires Python 3.3.x or higher
 
 from collections import defaultdict
 
@@ -37,7 +12,7 @@ from datetime import datetime
 import time
 import re
 from pymongo import MongoClient
-from statistics import mean, stdev, median
+from statistics import mean, stdev
 
 from auto_gen import currencyrates
 from auto_gen import hccurrencyrates
@@ -47,31 +22,6 @@ from auto_gen import phccurrencyrates
 header = '''#!/usr/bin/python
 # -*- coding: utf-8 -*-
 # Created: {} UTC from "{}" data
-"""
-* Copyright (c) 2016 Jeremy Parks. All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a
-* copy of this software and associated documentation files (the "Software"),
-* to deal in the Software without restriction, including without limitation
-* the rights to use, copy, modify, merge, publish, distribute, sublicense,
-* and/or sell copies of the Software, and to permit persons to whom the
-* Software is furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-* DEALINGS IN THE SOFTWARE.
-
-Author: Jeremy Parks
-Purpose: Create an item filter based on config files
-Note: Requires Python 3.4.x
-"""
 '''
 
 
@@ -144,6 +94,9 @@ def get_stashes(ldb, requester,start=None):
 				remove.append(ii['id'])
 				if 'items' in ii and ii['items']:
 					for iii in ii['items']:
+						# Why is SSF data in the stash tab river again?
+						if "SSF" in iii['league']:
+							break
 						if iii['frameType'] in [3, 6]:
 							note = ""
 							if 'note' in iii and ('~b/o' in iii['note'] or '~price' in iii['note']):
@@ -414,8 +367,8 @@ def poetrade_getcurrencyrates():
 	chaos = 4
 
 	defaults = {"exa": 45.0, "chaos": 1.0, "fuse": 0.5, "regal": 1, "alt": 1/16, "alch": 1/3, "jew": 1/8, "gcp": 1,
-				"divine": 15.0, "scour": 0.5, "blessed": 0.5, "vaal": 1, "chance": 1/14, "regret": 1.0, "chrom": 1/15,
-				"mirror": 20000.0, "chisel": 0.25, "silver": 0.333, "bauble": 1/16, "aug": 1/32, "transmute": 1/64, "perandus": 1/45,
+				"divine": 15.0, "scour": 0.5, "blessed": 0.5, "vaal": 1, "chance": 1/8, "regret": 1.0, "chrom": 1/15,
+				"mirror": 4500.0, "chisel": 0.25, "silver": 1/3, "bauble": 1/8, "aug": 1/32, "transmute": 1/64, "perandus": 1/45,
 				"apprenticecartosextant": .5, "journeycartosextant": 2, "mastercartosextant": 5}
 
 	with MongoClient() as client:
@@ -502,7 +455,7 @@ def poetrade_getcurrencyrates():
 
 
 if __name__ == '__main__':
-	#poetrade_getcurrencyrates()
+	poetrade_getcurrencyrates()
 	divuniqueupdate()
 
 
