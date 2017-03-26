4ffa836572bf411a4b758756280e65be272a4e24
xanthics <3137623+xanthics@users.noreply.github.com>
2017-03-26 16:22:00 -0700

    Market update

---
+++ auto_gen/hcessence.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Created: 03/26/2017(m/d/y) 23:19:46 UTC from "Hardcore" data
+
+desc = "Essence Autogen"
+
+# Base type : settings pair
+items = {
+	"0 Deafening Essence of Woe": {"base": "Deafening Essence of Woe", "class": "Currency", "type": "currency extremely high"},
+	"0 Essence of Delirium": {"base": "Essence of Delirium", "class": "Currency", "type": "currency extremely high"},
+	"0 Essence of Horror": {"base": "Essence of Horror", "class": "Currency", "type": "currency extremely high"},
+	"0 Essence of Hysteria": {"base": "Essence of Hysteria", "class": "Currency", "type": "currency extremely high"},
+	"0 Essence of Insanity": {"base": "Essence of Insanity", "class": "Currency", "type": "currency extremely high"},
+	"0 Remnant of Corruption": {"base": "Remnant of Corruption", "class": "Currency", "type": "currency extremely high"},
+	"7 Essence default": {"base": "Essence", "class": "Currency", "type": "currency low"}}
