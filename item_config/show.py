5017ced0fa5967fe3c23331ddd9150d5c0fa6fe4
xanthics <xanthic42@gmail.com>
2018-03-02 16:23:48 -0800

    Initial league tweaks

--- item_config/show.py
+++ item_config/show.py
@@ -13,8 +13,9 @@ items = {
 	# High value atlas bases.  Show and make noise at any rarity
 	"04 Steel Ring": {"other": ["PlayAlertSound 2 50"], "base": "Steel Ring", "type": "show high"},
 	"04 Crystal Belt": {"other": ["PlayAlertSound 2 50"], "base": "Crystal Belt", "type": "ignore"},
-	#"04 Stygian Vise": {"other": ["PlayAlertSound 2 50"], "base": "Stygian Vise", "type": "show high"},
+	"04 Stygian Vise": {"other": ["PlayAlertSound 2 50"], "base": "Stygian Vise", "type": "show high"},
 	"04 Opal Ring": {"other": ["PlayAlertSound 2 50"], "base": "Opal Ring", "type": "show high"},
+	"04 Abyss Jewel": {"class": "Abyss Jewel", "other": ["PlayAlertSound 2 50"], "type": "show high"},
 	"10 Talisman": {"class": "Amulets", 'other': ["Rarity >= Rare", "ItemLevel >= 70"], "base": "Talisman", "type": "show high"},
 	"11 Talisman": {"class": "Amulets", 'other': ["ItemLevel >= 70"], "base": "Talisman", "type": "show high"},
 	"12 Talisman": {"class": "Amulets", "base": "Talisman", "type": "show low"},
