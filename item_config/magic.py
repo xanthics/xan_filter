b3021614358d6a9265bf20a6e64c376f2f6c37e8
xanthics <3137623+xanthics@users.noreply.github.com>
2018-09-03 15:16:07 -0700

    Added new mods to show

--- item_config/magic.py
+++ item_config/magic.py
@@ -9,10 +9,12 @@ items = {
 	"0 magic item": {"other": ["Class Dagger Wand \"One Hand\" Bow Stave \"Two Hand\" Sceptre Claws", "Rarity Magic", "SetBorderColor 208 32 144", "Identified True"], "type": "hide"},
 }
 
+
 # TODO: expand this section for better creation of mods/bases to look at
 def magicmods():
-
-	ret = {"Magic Items": {"other": ['HasExplicitMod "Tacati\'s" "Citaqualotl\'s" "Matatl\'s" "Topotante\'s" "Xopec\'s" "Guatelitzi\'s" "of Tacati" "of Citaqualotl" "of Matatl" "of Puhuarte" "of Guatelitzi" "Brinerot" "Mutewind" "Redblade" "Betrayer\'s" "Deceiver\'s" "Turncoat\'s" "Tyrannical" "Merciless" '
-					 '"Electrocuting" "Resplendent" "Incandescent" "Rapturous" "Prime" "Vigorous" "Virile" "Overpowering" "of the Rainbow" "of Ephij" "of Haast" "of Tzteosh"'], "type": "show normal"}}
+	ret = {"Magic Items": {"other": ['HasExplicitMod "Tacati\'s" "Citaqualotl\'s" "Matatl\'s" "Topotante\'s" "Xopec\'s" "Guatelitzi\'s" "of Tacati" "of Citaqualotl" "of Matatl" '
+									 '"of Puhuarte" "of Guatelitzi" "Brinerot" "Mutewind" "Redblade" "Betrayer\'s" "Deceiver\'s" "Turncoat\'s" "Tyrannical" "Merciless" "Electrocuting" '
+									 '"Resplendent" "Incandescent" "Rapturous" "Prime" "Vigorous" "Virile" "Overpowering" "of the Rainbow" "of Ephij" "of Haast" "of Tzteosh" '
+									 '"Subterranean" "of the Underground" "of Weaponcraft" "of Spellcraft" "of Crafting"'], "type": "show normal"}}
 
 	return ret
