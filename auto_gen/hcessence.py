fc55e01c13ca3d373967860ac3ebd3d70f5fbba7
xanthics <xanthic42@gmail.com>
2018-10-10 03:56:56 -0700

    Initial base filtering commit

--- auto_gen/hcessence.py
+++ auto_gen/hcessence.py
@@ -1,57 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Created: 10/08/2018(m/d/y) 02:31:59 UTC from "Hardcore" data
+# Created: 10/10/2018(m/d/y) 10:53:00 UTC from "Hardcore" data
 
 desc = "Essence Autogen"
 
 # Base type : settings pair
 items = {
-	"0 Essence of Delirium": {"base": "Essence of Delirium", "class": "Currency", "type": "currency very high"},
-	"0 Essence of Hysteria": {"base": "Essence of Hysteria", "class": "Currency", "type": "currency high"},
 	"0 Essence of Insanity": {"base": "Essence of Insanity", "class": "Currency", "type": "currency extremely high"},
-	"0 Muttering Essence of Anger": {"base": "Muttering Essence of Anger", "class": "Currency", "type": "currency low"},
-	"0 Muttering Essence of Contempt": {"base": "Muttering Essence of Contempt", "class": "Currency", "type": "currency low"},
-	"0 Muttering Essence of Greed": {"base": "Muttering Essence of Greed", "class": "Currency", "type": "currency low"},
-	"0 Muttering Essence of Sorrow": {"base": "Muttering Essence of Sorrow", "class": "Currency", "type": "currency low"},
-	"0 Muttering Essence of Torment": {"base": "Muttering Essence of Torment", "class": "Currency", "type": "currency low"},
-	"0 Muttering Essence of Woe": {"base": "Muttering Essence of Woe", "class": "Currency", "type": "currency low"},
 	"0 Remnant of Corruption": {"base": "Remnant of Corruption", "class": "Currency", "type": "currency high"},
-	"0 Screaming Essence of Anger": {"base": "Screaming Essence of Anger", "class": "Currency", "type": "currency normal"},
-	"0 Screaming Essence of Anguish": {"base": "Screaming Essence of Anguish", "class": "Currency", "type": "currency normal"},
-	"0 Screaming Essence of Contempt": {"base": "Screaming Essence of Contempt", "class": "Currency", "type": "currency high"},
-	"0 Screaming Essence of Greed": {"base": "Screaming Essence of Greed", "class": "Currency", "type": "currency normal"},
-	"0 Screaming Essence of Hatred": {"base": "Screaming Essence of Hatred", "class": "Currency", "type": "currency normal"},
-	"0 Screaming Essence of Misery": {"base": "Screaming Essence of Misery", "class": "Currency", "type": "currency normal"},
-	"0 Screaming Essence of Sorrow": {"base": "Screaming Essence of Sorrow", "class": "Currency", "type": "currency normal"},
-	"0 Screaming Essence of Spite": {"base": "Screaming Essence of Spite", "class": "Currency", "type": "currency high"},
-	"0 Screaming Essence of Suffering": {"base": "Screaming Essence of Suffering", "class": "Currency", "type": "currency normal"},
-	"0 Screaming Essence of Torment": {"base": "Screaming Essence of Torment", "class": "Currency", "type": "currency normal"},
-	"0 Screaming Essence of Wrath": {"base": "Screaming Essence of Wrath", "class": "Currency", "type": "currency normal"},
-	"0 Shrieking Essence of Dread": {"base": "Shrieking Essence of Dread", "class": "Currency", "type": "currency normal"},
-	"0 Wailing Essence of Anger": {"base": "Wailing Essence of Anger", "class": "Currency", "type": "currency normal"},
-	"0 Wailing Essence of Anguish": {"base": "Wailing Essence of Anguish", "class": "Currency", "type": "currency low"},
-	"0 Wailing Essence of Contempt": {"base": "Wailing Essence of Contempt", "class": "Currency", "type": "currency low"},
-	"0 Wailing Essence of Fear": {"base": "Wailing Essence of Fear", "class": "Currency", "type": "currency low"},
-	"0 Wailing Essence of Greed": {"base": "Wailing Essence of Greed", "class": "Currency", "type": "currency low"},
-	"0 Wailing Essence of Hatred": {"base": "Wailing Essence of Hatred", "class": "Currency", "type": "currency normal"},
-	"0 Wailing Essence of Loathing": {"base": "Wailing Essence of Loathing", "class": "Currency", "type": "currency low"},
-	"0 Wailing Essence of Sorrow": {"base": "Wailing Essence of Sorrow", "class": "Currency", "type": "currency low"},
-	"0 Wailing Essence of Spite": {"base": "Wailing Essence of Spite", "class": "Currency", "type": "currency low"},
-	"0 Wailing Essence of Suffering": {"base": "Wailing Essence of Suffering", "class": "Currency", "type": "currency low"},
-	"0 Wailing Essence of Torment": {"base": "Wailing Essence of Torment", "class": "Currency", "type": "currency low"},
-	"0 Wailing Essence of Woe": {"base": "Wailing Essence of Woe", "class": "Currency", "type": "currency low"},
-	"0 Wailing Essence of Zeal": {"base": "Wailing Essence of Zeal", "class": "Currency", "type": "currency low"},
-	"0 Weeping Essence of Anger": {"base": "Weeping Essence of Anger", "class": "Currency", "type": "currency low"},
-	"0 Weeping Essence of Doubt": {"base": "Weeping Essence of Doubt", "class": "Currency", "type": "currency low"},
-	"0 Weeping Essence of Fear": {"base": "Weeping Essence of Fear", "class": "Currency", "type": "currency low"},
-	"0 Weeping Essence of Greed": {"base": "Weeping Essence of Greed", "class": "Currency", "type": "currency low"},
-	"0 Weeping Essence of Hatred": {"base": "Weeping Essence of Hatred", "class": "Currency", "type": "currency low"},
-	"0 Weeping Essence of Sorrow": {"base": "Weeping Essence of Sorrow", "class": "Currency", "type": "currency low"},
-	"0 Weeping Essence of Suffering": {"base": "Weeping Essence of Suffering", "class": "Currency", "type": "currency normal"},
-	"0 Weeping Essence of Torment": {"base": "Weeping Essence of Torment", "class": "Currency", "type": "currency low"},
-	"0 Weeping Essence of Woe": {"base": "Weeping Essence of Woe", "class": "Currency", "type": "currency low"},
-	"0 Weeping Essence of Wrath": {"base": "Weeping Essence of Wrath", "class": "Currency", "type": "currency low"},
-	"0 Whispering Essence of Contempt": {"base": "Whispering Essence of Contempt", "class": "Currency", "type": "currency low"},
-	"0 Whispering Essence of Greed": {"base": "Whispering Essence of Greed", "class": "Currency", "type": "currency low"},
-	"0 Whispering Essence of Woe": {"base": "Whispering Essence of Woe", "class": "Currency", "type": "currency low"},
 	"7 Essence default": {"base": "Essence", "class": "Currency", "type": "currency low"}}
---
+++ auto_gen/thcbases.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Created: 10/10/2018(m/d/y) 10:53:03 UTC from "tmphardcore" data
+
+desc = "Bases"
+
+# Base type : settings pair
+items = {
+	"0 Shaper Onyx Amulet 85": {"base": "Onyx Amulet", "other": ["Shaper True", "Itemlevel >= 85"], "type": "base extremely high"},
+	"0 Elder Leather Belt 83": {"base": "Leather Belt", "other": ["Elder True", "Itemlevel >= 83"], "type": "base extremely high"},
+	"1 Crystal Belt 84": {"base": "Crystal Belt", "other": ["Itemlevel >= 84"], "type": "base very high"},
+	"1 Hydrascale Boots 86": {"base": "Hydrascale Boots", "other": ["Itemlevel >= 86"], "type": "base very high"},
+	"1 Hypnotic Eye Jewel 84": {"base": "Hypnotic Eye Jewel", "other": ["Itemlevel >= 84"], "type": "base very high"},
+	"1 Marble Amulet 83": {"base": "Marble Amulet", "other": ["Itemlevel >= 83"], "type": "base very high"},
+	"1 Murderous Eye Jewel 84": {"base": "Murderous Eye Jewel", "other": ["Itemlevel >= 84"], "type": "base very high"},
+	"1 Opal Ring 82": {"base": "Opal Ring", "other": ["Itemlevel >= 82"], "type": "base very high"},
+	"1 Searching Eye Jewel 82": {"base": "Searching Eye Jewel", "other": ["Itemlevel >= 82"], "type": "base very high"},
+	"1 Stygian Vise 85": {"base": "Stygian Vise", "other": ["Itemlevel >= 85"], "type": "base very high"},
+	"1 Titan Greaves 86": {"base": "Titan Greaves", "other": ["Itemlevel >= 86"], "type": "base very high"},
+	"1 Titanium Spirit Shield 86": {"base": "Titanium Spirit Shield", "other": ["Itemlevel >= 86"], "type": "base very high"},
+	"1 Elder Heavy Belt 83": {"base": "Heavy Belt", "other": ["Elder True", "Itemlevel >= 83"], "type": "base very high"},
+	"1 Elder Onyx Amulet 83": {"base": "Onyx Amulet", "other": ["Elder True", "Itemlevel >= 83"], "type": "base very high"},
+	"1 Elder Ruby Ring 83": {"base": "Ruby Ring", "other": ["Elder True", "Itemlevel >= 83"], "type": "base very high"},
+	"1 Elder Two-Stone Ring 83": {"base": "Two-Stone Ring", "other": ["Elder True", "Itemlevel >= 83"], "type": "base very high"},
+	"1 Shaper Harpy Rapier 83": {"base": "Harpy Rapier", "other": ["Shaper True", "Itemlevel >= 83"], "type": "base very high"},
+	"1 Shaper Heavy Belt 82": {"base": "Heavy Belt", "other": ["Shaper True", "Itemlevel >= 82"], "type": "base very high"},
+	"1 Shaper Leather Belt 83": {"base": "Leather Belt", "other": ["Shaper True", "Itemlevel >= 83"], "type": "base very high"},
+	"1 Shaper Onyx Amulet 82": {"base": "Onyx Amulet", "other": ["Shaper True", "Itemlevel >= 82"], "type": "base very high"},
+	"1 Shaper Two-Stone Ring 83": {"base": "Two-Stone Ring", "other": ["Shaper True", "Itemlevel >= 83"], "type": "base very high"},
+	"1 Shaper Tyrant's Sekhem 83": {"base": "Tyrant's Sekhem", "other": ["Shaper True", "Itemlevel >= 83"], "type": "base very high"},
+
+}
