1f2d1eaafe94d2882e199b3ad4af4eb970dd4d76
xanthics <xanthic42@gmail.com>
2018-10-21 21:25:39 -0700

    Market update.

--- auto_gen/hcessence.py
+++ auto_gen/hcessence.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Created: 10/17/2018(m/d/y) 17:20:43 UTC from "Hardcore" data
+# Created: 10/22/2018(m/d/y) 04:25:18 UTC from "Hardcore" data
 
 desc = "Essence Autogen"
 
 # Base type : settings pair
 items = {
+	"0 Essence of Delirium": {"base": "Essence of Delirium", "class": "Currency", "type": "currency extremely high"},
 	"0 Essence of Horror": {"base": "Essence of Horror", "class": "Currency", "type": "currency extremely high"},
+	"0 Essence of Insanity": {"base": "Essence of Insanity", "class": "Currency", "type": "currency extremely high"},
+	"0 Muttering Essence of Anger": {"base": "Muttering Essence of Anger", "class": "Currency", "type": "currency normal"},
+	"0 Muttering Essence of Greed": {"base": "Muttering Essence of Greed", "class": "Currency", "type": "currency normal"},
 	"0 Remnant of Corruption": {"base": "Remnant of Corruption", "class": "Currency", "type": "currency high"},
+	"0 Wailing Essence of Sorrow": {"base": "Wailing Essence of Sorrow", "class": "Currency", "type": "currency normal"},
+	"0 Weeping Essence of Hatred": {"base": "Weeping Essence of Hatred", "class": "Currency", "type": "currency normal"},
+	"0 Weeping Essence of Rage": {"base": "Weeping Essence of Rage", "class": "Currency", "type": "currency normal"},
 	"7 Essence default": {"base": "Essence", "class": "Currency", "type": "currency low"}}
