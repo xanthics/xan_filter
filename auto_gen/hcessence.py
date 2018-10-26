b4e1edae8dffc706a5488f6a3dac8e7bf73892b1
xanthics <xanthic42@gmail.com>
2018-10-26 11:16:02 -0700

    Market update.

--- auto_gen/hcessence.py
+++ auto_gen/hcessence.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Created: 10/22/2018(m/d/y) 05:00:41 UTC from "Hardcore" data
+# Created: 10/26/2018(m/d/y) 18:11:54 UTC from "Hardcore" data
 
 desc = "Essence Autogen"
 
@@ -9,10 +9,4 @@ items = {
 	"0 Essence of Delirium": {"base": "Essence of Delirium", "class": "Currency", "type": "currency extremely high"},
 	"0 Essence of Horror": {"base": "Essence of Horror", "class": "Currency", "type": "currency extremely high"},
 	"0 Essence of Insanity": {"base": "Essence of Insanity", "class": "Currency", "type": "currency extremely high"},
-	"0 Muttering Essence of Anger": {"base": "Muttering Essence of Anger", "class": "Currency", "type": "currency normal"},
-	"0 Muttering Essence of Greed": {"base": "Muttering Essence of Greed", "class": "Currency", "type": "currency normal"},
-	"0 Remnant of Corruption": {"base": "Remnant of Corruption", "class": "Currency", "type": "currency high"},
-	"0 Wailing Essence of Sorrow": {"base": "Wailing Essence of Sorrow", "class": "Currency", "type": "currency normal"},
-	"0 Weeping Essence of Hatred": {"base": "Weeping Essence of Hatred", "class": "Currency", "type": "currency normal"},
-	"0 Weeping Essence of Rage": {"base": "Weeping Essence of Rage", "class": "Currency", "type": "currency normal"},
 	"7 Essence default": {"base": "Essence", "class": "Currency", "type": "currency low"}}
--- auto_gen/thcbases.py
+++ auto_gen/thcbases.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Created: 10/22/2018(m/d/y) 05:00:53 UTC from "tmphardcore" data
+# Created: 10/26/2018(m/d/y) 18:12:06 UTC from "tmphardcore" data
 
 desc = "Bases"
 
 # Base type : settings pair
 items = {
+	"0 Sorcerer Boots": {"base": "Sorcerer Boots", "other": ["ItemLevel >= 86"], "type": "base very high"},
 	"0 Steel Ring": {"base": "Steel Ring", "other": ["ItemLevel >= 86"], "type": "base very high"},
 	"0 Stygian Vise": {"base": "Stygian Vise", "other": ["ItemLevel >= 86"], "type": "base very high"},
 	"0 Titanium Spirit Shield": {"base": "Titanium Spirit Shield", "other": ["ItemLevel >= 86"], "type": "base very high"},
 	"0 Two-Toned Boots": {"base": "Two-Toned Boots", "other": ["ItemLevel >= 86"], "type": "base very high"},
 	"1 Stygian Vise": {"base": "Stygian Vise", "other": ["ItemLevel >= 85"], "type": "base very high"},
-	"2 Murderous Eye Jewel": {"base": "Murderous Eye Jewel", "other": ["ItemLevel >= 84"], "type": "base very high"},
 	"2 Opal Ring": {"base": "Opal Ring", "other": ["ItemLevel >= 84"], "type": "base very high"},
 	"2 Stygian Vise": {"base": "Stygian Vise", "other": ["ItemLevel >= 84"], "type": "base very high"},
+	"3 Shaper Heavy Belt": {"base": "Heavy Belt", "other": ["ShaperItem True", "ItemLevel >= 83"], "type": "base very high"},
 	"3 Elder Heavy Belt": {"base": "Heavy Belt", "other": ["ElderItem True", "ItemLevel >= 83"], "type": "base very high"},
-	"3 Elder Two-Stone Ring": {"base": "Two-Stone Ring", "other": ["ElderItem True", "ItemLevel >= 83"], "type": "base very high"},
+	"3 Hypnotic Eye Jewel": {"base": "Hypnotic Eye Jewel", "other": ["ItemLevel >= 83"], "type": "base very high"},
 	"3 Marble Amulet": {"base": "Marble Amulet", "other": ["ItemLevel >= 83"], "type": "base very high"},
 	"3 Murderous Eye Jewel": {"base": "Murderous Eye Jewel", "other": ["ItemLevel >= 83"], "type": "base very high"},
 	"3 Opal Ring": {"base": "Opal Ring", "other": ["ItemLevel >= 83"], "type": "base very high"},
 	"3 Searching Eye Jewel": {"base": "Searching Eye Jewel", "other": ["ItemLevel >= 83"], "type": "base very high"},
+	"4 Shaper Onyx Amulet": {"base": "Onyx Amulet", "other": ["ShaperItem True", "ItemLevel >= 82"], "type": "base very high"},
 	"4 Hypnotic Eye Jewel": {"base": "Hypnotic Eye Jewel", "other": ["ItemLevel >= 82"], "type": "base very high"},
+	"4 Murderous Eye Jewel": {"base": "Murderous Eye Jewel", "other": ["ItemLevel >= 82"], "type": "base very high"},
+	"4 Opal Ring": {"base": "Opal Ring", "other": ["ItemLevel >= 82"], "type": "base very high"},
 	"4 Searching Eye Jewel": {"base": "Searching Eye Jewel", "other": ["ItemLevel >= 82"], "type": "base very high"},
 
 }
