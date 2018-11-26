3eb610e9c83d8df1c70d3869ce7cd27a755116fc
xanthics <3137623+xanthics@users.noreply.github.com>
2018-11-26 12:46:22 -0800

    Tweaked 3l highlighting to stop at ilvl 40 for wand/sceptre

--- item_config/general_levelling.py
+++ item_config/general_levelling.py
@@ -17,7 +17,9 @@ items = {
 	"1 linked 25-50": {"other": ["Rarity <= Magic", "DropLevel >= 25", "ItemLevel <= 50", "LinkedSockets > 3"], "type": "leveling normal"},
 	"1 linked 40-65": {"other": ["Rarity <= Magic", "DropLevel >= 40", "ItemLevel <= 65", "LinkedSockets > 3"], "type": "leveling normal"},
 
-	"1 Sceptre/Wand linked 15-40": {"class": "Sceptres\" \"Wands", "other": ["Rarity <= Magic", "DropLevel >= 15", "ItemLevel <= 40", "LinkedSockets >= 3"], "type": "leveling normal"},
-	"1 Sceptre/Wand linked 25-50": {"class": "Sceptres\" \"Wands", "other": ["Rarity <= Magic", "DropLevel >= 25", "ItemLevel <= 50", "LinkedSockets >= 3"], "type": "leveling normal"},
-	"1 Sceptre/Wand linked 40-65": {"class": "Sceptres\" \"Wands", "other": ["Rarity <= Magic", "DropLevel >= 40", "ItemLevel <= 65", "LinkedSockets >= 3"], "type": "leveling normal"},
+	"1 Sceptre/Wand linked 15-40": {"class": "Sceptres\" \"Wands", "other": ["Rarity <= Magic", "ItemLevel <= 40", "LinkedSockets >= 3"], "type": "leveling normal"},
+
+#	"1 Sceptre/Wand linked 15-40": {"class": "Sceptres\" \"Wands", "other": ["Rarity <= Magic", "DropLevel >= 15", "ItemLevel <= 40", "LinkedSockets >= 3"], "type": "leveling normal"},
+#	"1 Sceptre/Wand linked 25-50": {"class": "Sceptres\" \"Wands", "other": ["Rarity <= Magic", "DropLevel >= 25", "ItemLevel <= 50", "LinkedSockets >= 3"], "type": "leveling normal"},
+#	"1 Sceptre/Wand linked 40-65": {"class": "Sceptres\" \"Wands", "other": ["Rarity <= Magic", "DropLevel >= 40", "ItemLevel <= 65", "LinkedSockets >= 3"], "type": "leveling normal"},
 }
