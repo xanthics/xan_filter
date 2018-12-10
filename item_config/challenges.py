480bbedd188d0b145fa06006090041d9c01c6ded
xanthics <3137623+xanthics@users.noreply.github.com>
2018-12-10 03:11:41 -0800

    Minor Bug Fix

--- item_config/challenges.py
+++ item_config/challenges.py
@@ -6,5 +6,5 @@ desc = "challenge item"
 
 # Base type : settings pair
 items = {
-	"0 Scarabs": {"base": "Scarab", "class": "Map Fragments", "type": "challenge normal"},
+#	"0 Scarabs": {"base": "Scarab", "class": "Map Fragments", "type": "challenge normal"},
 }
