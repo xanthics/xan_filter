4e052bad6992dec518ccc6bae5b3ba238116e56d
xanthics <3137623+xanthics@users.noreply.github.com>
2019-06-24 21:12:57 -0700

    Market Update

--- auto_gen/tincubator.py
+++ auto_gen/tincubator.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Created: 06/09/2019(m/d/y) 20:49:00 UTC from "tmpstandard" data
+# Created: 06/25/2019(m/d/y) 04:12:38 UTC from "tmpstandard" data
 
 desc = "Incubator Autogen"
 
 # Base type : settings pair
 items = {
-	"0 Cartographer's Incubator": {"base": "Cartographer's Incubator", "class": "Incubator", "type": "currency high"},
 	"0 Celestial Armoursmith's Incubator": {"base": "Celestial Armoursmith's Incubator", "class": "Incubator", "type": "currency high"},
 	"0 Celestial Blacksmith's Incubator": {"base": "Celestial Blacksmith's Incubator", "class": "Incubator", "type": "currency high"},
 	"0 Celestial Jeweller's Incubator": {"base": "Celestial Jeweller's Incubator", "class": "Incubator", "type": "currency high"},
 	"0 Decadent Incubator": {"base": "Decadent Incubator", "class": "Incubator", "type": "currency high"},
-	"0 Enchanted Incubator": {"base": "Enchanted Incubator", "class": "Incubator", "type": "currency high"},
+	"0 Diviner's Incubator": {"base": "Diviner's Incubator", "class": "Incubator", "type": "currency high"},
+	"0 Eldritch Incubator": {"base": "Eldritch Incubator", "class": "Incubator", "type": "currency high"},
 	"0 Foreboding Incubator": {"base": "Foreboding Incubator", "class": "Incubator", "type": "currency high"},
 	"0 Geomancer's Incubator": {"base": "Geomancer's Incubator", "class": "Incubator", "type": "currency very high"},
+	"0 Infused Incubator": {"base": "Infused Incubator", "class": "Incubator", "type": "currency high"},
+	"0 Mysterious Incubator": {"base": "Mysterious Incubator", "class": "Incubator", "type": "currency high"},
 	"0 Obscured Incubator": {"base": "Obscured Incubator", "class": "Incubator", "type": "currency high"},
+	"0 Ornate Incubator": {"base": "Ornate Incubator", "class": "Incubator", "type": "currency high"},
 	"0 Otherworldly Incubator": {"base": "Otherworldly Incubator", "class": "Incubator", "type": "currency high"},
-	"0 Thaumaturge's Incubator": {"base": "Thaumaturge's Incubator", "class": "Incubator", "type": "currency very high"},
+	"0 Skittering Incubator": {"base": "Skittering Incubator", "class": "Incubator", "type": "currency high"},
+	"0 Thaumaturge's Incubator": {"base": "Thaumaturge's Incubator", "class": "Incubator", "type": "currency high"},
 	"0 Time-Lost Incubator": {"base": "Time-Lost Incubator", "class": "Incubator", "type": "currency very high"},
 	"7 incubator default": {"base": "Incubator", "class": "Incubator", "type": "currency normal"}}
