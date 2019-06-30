f635250727f8b5502cd7af1d507157e062bd35ba
xanthics <3137623+xanthics@users.noreply.github.com>
2019-06-29 21:53:49 -0700

    Market update

--- auto_gen/thelmenchant.py
+++ auto_gen/thelmenchant.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Created: 06/26/2019(m/d/y) 21:02:11 UTC from "tmpstandard" data
+# Created: 06/30/2019(m/d/y) 04:53:27 UTC from "tmpstandard" data
 
 desc = "Helm Enchant Autogen"
 
 # Base type : settings pair
 items = {
 	"000 Enchantment Flame Surge Vs Burning Enemies 1": {"enchant": "Enchantment Flame Surge Vs Burning Enemies 1", "type": "ignore"},
+	"1 10% increased Cyclone Attack Speed": {"enchant": "Enchantment Cyclone Attack Speed 1", "type": "currency very high"},
 	"1 15% increased Cyclone Attack Speed": {"enchant": "Enchantment Cyclone Attack Speed 2", "type": "currency extremely high"},
-	"1 30% increased Temporal Chains Curse Effect": {"enchant": "Enchantment Temporal Chains Curse Effect 2", "type": "currency very high"},
+	"1 30% increased Spark Projectile Speed": {"enchant": "Enchantment Spark Projectile Speed 2", "type": "currency very high"},
+	"1 30% increased Temporal Chains Curse Effect": {"enchant": "Enchantment Temporal Chains Curse Effect 2", "type": "currency extremely high"},
 	"1 30% reduced Earthquake Duration": {"enchant": "Enchantment Earthquake Duration 2", "type": "currency very high"},
 	"1 30% reduced Storm Call Duration": {"enchant": "Enchantment Storm Call Duration 2", "type": "currency very high"},
 	"1 40% increased Cyclone Damage": {"enchant": "Enchantment Cyclone Damage 2", "type": "currency extremely high"},
-	"1 40% increased Essence Drain Damage": {"enchant": "Enchantment Essence Drain Damage 2", "type": "currency extremely high"},
+	"1 40% increased Essence Drain Damage": {"enchant": "Enchantment Essence Drain Damage 2", "type": "currency very high"},
 	"1 40% increased Frost Blades Damage": {"enchant": "Enchantment Frost Blades Damage 2", "type": "currency very high"},
 	"1 40% increased Ice Nova Damage": {"enchant": "Enchantment Ice Nova Damage 2", "type": "currency very high"},
+	"1 9% increased Flicker Strike Damage per Frenzy Charge": {"enchant": "Enchantment Flicker Strike Damage Per Frenzy Charge 2", "type": "currency very high"},
 	"1 Arc Chains an additional time": {"enchant": "Enchantment Arc Num Of Additional Projectiles In Chain 1", "type": "currency very high"},
 	"1 Barrage fires an additional Projectile": {"enchant": "Enchantment Barrage Num Of Additional Projectiles 1", "type": "currency very high"},
 	"1 Blood Rage grants additional 12% increased Attack Speed": {"enchant": "Enchantment Blood Rage Attack Speed 2", "type": "currency very high"},
@@ -22,7 +25,8 @@ items = {
 	"1 Lightning Trap Damage Penetrates 10% Lightning Resistance": {"enchant": "Enchantment Lightning Trap Penetration 2", "type": "currency very high"},
 	"1 Magma Orb Chains an additional 2 times": {"enchant": "Enchantment Magma Orb Num Of Additional Projectiles In Chain 2", "type": "currency very high"},
 	"1 Malevolence has 15% reduced Mana Reservation": {"enchant": "Enchantment Malevolence Mana Reservation 2", "type": "currency very high"},
-	"1 Scourge Arrow creates an additional spore pod at Maximum Stages": {"enchant": "Enchantment Scourge Arrow Additional Spore 1", "type": "currency very high"},
+	"1 Scourge Arrow creates an additional spore pod at Maximum Stages": {"enchant": "Enchantment Scourge Arrow Additional Spore 1", "type": "currency extremely high"},
+	"1 Spark fires 3 additional Projectiles": {"enchant": "Enchantment Spark Num Of Additional Projectiles 2", "type": "currency very high"},
 	"1 Spectral Shield Throw fires 5 additional Shard Projectiles": {"enchant": "Enchantment Spectral Shield Throw Num Of Additional Projectiles 2", "type": "currency very high"},
 	"1 Tectonic Slam has 20% chance to create a Charged Slam": {"enchant": "Enchantment Tectonic Slam Chance To Charged Slam 2", "type": "currency very high"},
 	"1 Tornado Shot fires 2 additional secondary Projectiles": {"enchant": "Enchantment Tornado Shot Num Of Secondary Projectiles 2", "type": "currency extremely high"},
