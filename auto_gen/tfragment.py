99fdb8941156daa59237bc38ec049577e1951427
xanthics <3137623+xanthics@users.noreply.github.com>
2019-08-03 18:19:02 -0700

    Market Update

--- auto_gen/tfragment.py
+++ auto_gen/tfragment.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Created: 07/17/2019(m/d/y) 21:51:32 UTC from "tmpstandard" data
+# Created: 08/04/2019(m/d/y) 01:18:10 UTC from "tmpstandard" data
 
 desc = "Fragment Autogen"
 
@@ -14,7 +14,7 @@ items = {
 	"1 Eber's Key": {"base": "Eber's Key", "class": "Map Fragments", "type": "fragment high"},
 	"1 Esh's Breachstone": {"base": "Esh's Breachstone", "class": "Map Fragments", "type": "fragment normal"},
 	"1 Esh's Charged Breachstone": {"base": "Esh's Charged Breachstone", "class": "Map Fragments", "type": "fragment normal"},
-	"1 Esh's Enriched Breachstone": {"base": "Esh's Enriched Breachstone", "class": "Map Fragments", "type": "fragment high"},
+	"1 Esh's Enriched Breachstone": {"base": "Esh's Enriched Breachstone", "class": "Map Fragments", "type": "fragment very high"},
 	"1 Esh's Pure Breachstone": {"base": "Esh's Pure Breachstone", "class": "Map Fragments", "type": "fragment very high"},
 	"1 Fragment of the Chimera": {"base": "Fragment of the Chimera", "class": "Map Fragments", "type": "fragment high"},
 	"1 Fragment of the Hydra": {"base": "Fragment of the Hydra", "class": "Map Fragments", "type": "fragment high"},
@@ -31,17 +31,17 @@ items = {
 	"1 Sacrifice at Midnight": {"base": "Sacrifice at Midnight", "class": "Map Fragments", "type": "fragment normal"},
 	"1 Sacrifice at Noon": {"base": "Sacrifice at Noon", "class": "Map Fragments", "type": "fragment normal"},
 	"1 Tul's Breachstone": {"base": "Tul's Breachstone", "class": "Map Fragments", "type": "fragment normal"},
-	"1 Tul's Charged Breachstone": {"base": "Tul's Charged Breachstone", "class": "Map Fragments", "type": "fragment high"},
+	"1 Tul's Charged Breachstone": {"base": "Tul's Charged Breachstone", "class": "Map Fragments", "type": "fragment normal"},
 	"1 Tul's Enriched Breachstone": {"base": "Tul's Enriched Breachstone", "class": "Map Fragments", "type": "fragment very high"},
 	"1 Tul's Pure Breachstone": {"base": "Tul's Pure Breachstone", "class": "Map Fragments", "type": "fragment very high"},
 	"1 Uul-Netol's Breachstone": {"base": "Uul-Netol's Breachstone", "class": "Map Fragments", "type": "fragment normal"},
-	"1 Uul-Netol's Charged Breachstone": {"base": "Uul-Netol's Charged Breachstone", "class": "Map Fragments", "type": "fragment high"},
+	"1 Uul-Netol's Charged Breachstone": {"base": "Uul-Netol's Charged Breachstone", "class": "Map Fragments", "type": "fragment very high"},
 	"1 Uul-Netol's Enriched Breachstone": {"base": "Uul-Netol's Enriched Breachstone", "class": "Map Fragments", "type": "fragment very high"},
 	"1 Uul-Netol's Pure Breachstone": {"base": "Uul-Netol's Pure Breachstone", "class": "Map Fragments", "type": "fragment very high"},
 	"1 Volkuur's Key": {"base": "Volkuur's Key", "class": "Map Fragments", "type": "fragment high"},
 	"1 Xoph's Breachstone": {"base": "Xoph's Breachstone", "class": "Map Fragments", "type": "fragment normal"},
 	"1 Xoph's Charged Breachstone": {"base": "Xoph's Charged Breachstone", "class": "Map Fragments", "type": "fragment normal"},
-	"1 Xoph's Enriched Breachstone": {"base": "Xoph's Enriched Breachstone", "class": "Map Fragments", "type": "fragment high"},
+	"1 Xoph's Enriched Breachstone": {"base": "Xoph's Enriched Breachstone", "class": "Map Fragments", "type": "fragment very high"},
 	"1 Xoph's Pure Breachstone": {"base": "Xoph's Pure Breachstone", "class": "Map Fragments", "type": "fragment very high"},
 	"1 Yriel's Key": {"base": "Yriel's Key", "class": "Map Fragments", "type": "fragment high"},
 }
--- auto_gen/thelmenchant.py
+++ auto_gen/thelmenchant.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Created: 07/17/2019(m/d/y) 21:51:32 UTC from "tmpstandard" data
+# Created: 08/04/2019(m/d/y) 01:18:10 UTC from "tmpstandard" data
 
 desc = "Helm Enchant Autogen"
 
@@ -9,10 +9,12 @@ items = {
 	"000 Enchantment Flame Surge Vs Burning Enemies 1": {"enchant": "Enchantment Flame Surge Vs Burning Enemies 1", "type": "ignore"},
 	"1 15% increased Cyclone Attack Speed": {"enchant": "Enchantment Cyclone Attack Speed 2", "type": "base extremely high"},
 	"1 30% increased Essence Drain Duration": {"enchant": "Enchantment Essence Drain Duration 2", "type": "base very high"},
+	"1 30% increased Freezing Pulse Projectile Speed": {"enchant": "Enchantment Freezing Pulse Projectile Speed 2", "type": "base very high"},
 	"1 30% increased Temporal Chains Curse Effect": {"enchant": "Enchantment Temporal Chains Curse Effect 2", "type": "base extremely high"},
-	"1 30% increased Vengeance Cooldown Recovery Speed": {"enchant": "Enchantment Vengeance Cooldown Speed 2", "type": "base very high"},
+	"1 30% increased Vengeance Cooldown Recovery Speed": {"enchant": "Enchantment Vengeance Cooldown Speed 2", "type": "base extremely high"},
 	"1 30% reduced Earthquake Duration": {"enchant": "Enchantment Earthquake Duration 2", "type": "base very high"},
 	"1 30% reduced Storm Call Duration": {"enchant": "Enchantment Storm Call Duration 2", "type": "base very high"},
+	"1 40% increased Arc Damage": {"enchant": "Enchantment Arc Damage 2", "type": "base very high"},
 	"1 40% increased Cyclone Damage": {"enchant": "Enchantment Cyclone Damage 2", "type": "base extremely high"},
 	"1 40% increased Essence Drain Damage": {"enchant": "Enchantment Essence Drain Damage 2", "type": "base extremely high"},
 	"1 40% increased Flicker Strike Damage": {"enchant": "Enchantment Flicker Strike Damage 2", "type": "base very high"},
@@ -22,19 +24,19 @@ items = {
 	"1 Arc Chains an additional time": {"enchant": "Enchantment Arc Num Of Additional Projectiles In Chain 1", "type": "base very high"},
 	"1 Barrage fires an additional Projectile": {"enchant": "Enchantment Barrage Num Of Additional Projectiles 1", "type": "base very high"},
 	"1 Blood Rage grants additional 12% increased Attack Speed": {"enchant": "Enchantment Blood Rage Attack Speed 2", "type": "base very high"},
+	"1 Ice Spear fires an additional Projectile": {"enchant": "Enchantment Ice Spear Additional Projectile 1", "type": "base very high"},
 	"1 Lightning Trap Damage Penetrates 10% Lightning Resistance": {"enchant": "Enchantment Lightning Trap Penetration 2", "type": "base very high"},
 	"1 Magma Orb Chains an additional 2 times": {"enchant": "Enchantment Magma Orb Num Of Additional Projectiles In Chain 2", "type": "base very high"},
 	"1 Molten Strike fires 2 additional Projectiles": {"enchant": "Enchantment Molten Strike Num Of Additional Projectiles 2", "type": "base very high"},
 	"1 Scourge Arrow creates an additional spore pod at Maximum Stages": {"enchant": "Enchantment Scourge Arrow Additional Spore 1", "type": "base extremely high"},
 	"1 Spectral Shield Throw fires 5 additional Shard Projectiles": {"enchant": "Enchantment Spectral Shield Throw Num Of Additional Projectiles 2", "type": "base very high"},
+	"1 Storm Brand Damage Penetrates 12% of Branded Enemy's Lightning Resistance": {"enchant": "Storm Brand Attached Target Lightning Penetration 2", "type": "base very high"},
 	"1 Tectonic Slam has 20% chance to create a Charged Slam": {"enchant": "Enchantment Tectonic Slam Chance To Charged Slam 2", "type": "base very high"},
 	"1 Tornado Shot fires 2 additional secondary Projectiles": {"enchant": "Enchantment Tornado Shot Num Of Secondary Projectiles 2", "type": "base extremely high"},
 	"1 Tornado Shot fires an additional secondary Projectile": {"enchant": "Enchantment Tornado Shot Num Of Secondary Projectiles 1", "type": "base very high"},
-	"1 Toxic Rain deals 40% increased Damage": {"enchant": "Enchantment Toxic Rain Damage 2", "type": "base very high"},
 	"1 Toxic Rain fires 1 additional Arrow": {"enchant": "Enchantment Toxic Rain Num Of Additional Projectiles 1", "type": "base very high"},
 	"1 0 Volatile Dead destroys up to 1 additional Corpse": {"enchant": "Enchantment Volatile Dead Orbs 1", "type": "base very high"},
 	"1 1 Volatile Dead destroys up to 1 additional Corpse": {"enchant": "Enchantment Volatile Dead Orbs 2", "type": "base very high"},
 	"1 2 Volatile Dead destroys up to 1 additional Corpse": {"enchant": "Enchantment Volatile Dead Orbs 3", "type": "base very high"},
-	"1 Winter Orb has +2 Maximum Stages": {"enchant": "Enchantment Frost Fury Additional Max Number Of Stages 1", "type": "base very high"},
 	"7 enchant default": {"class": "Helmet", "other": ["AnyEnchantment True"], "type": "item mod"}
 }
