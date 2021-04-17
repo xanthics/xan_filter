3d2956ce1e3ec2951252801007c59810e52cf9e5
xanthics <3137623+xanthics@users.noreply.github.com>
2021-04-16 19:06:05 -0700

    Updated for new league

--- item_config/challenges.py
+++ item_config/challenges.py
@@ -9,16 +9,6 @@ desc = "challenge item"
 # Base type : settings pair
 items = {
 	"1 Cluster Jewel": {"base": "Cluster Jewel", "class": "Jewel", "type": "show normal"},
-	# ritual bases for challenge
-#	"1 Archdemon Crown": {"baseexact": "Archdemon Crown", "type": "show normal"},
-	"1 Blizzard Crown": {"baseexact": "Blizzard Crown", "type": "show normal"},
-#	"1 Brimstone Treads": {"baseexact": "Brimstone Treads", "type": "show normal"},
-#	"1 Debilitation Gauntlets": {"baseexact": "Debilitation Gauntlets", "type": "show normal"},
-	"1 Dreamquest Slippers": {"baseexact": "Dreamquest Slippers", "type": "show normal"},
-#	"1 Nexus Gloves": {"baseexact": "Nexus Gloves", "type": "show normal"},
-#	"1 Penitent Mask": {"baseexact": "Penitent Mask", "type": "show normal"},
-#	"1 Sinistral Gloves": {"baseexact": "Sinistral Gloves", "type": "show normal"},
-#	"1 Stormrider Boots": {"baseexact": "Stormrider Boots", "type": "show normal"},
 
 	# initial Heist highlighting
 	"1 Heist Any 83+": {"class": "Heist", "other": ["ItemLevel >= 83"], "type": "challenge normal"},
