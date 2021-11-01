d41cea7805558aa21709b4ab9002acaca110a36d
xanthics <3137623+xanthics@users.noreply.github.com>
2021-11-01 07:09:09 -0700

    20q+ gems to normal currency alert

--- item_config/gems.py
+++ item_config/gems.py
@@ -6,7 +6,7 @@ desc = "gems"
 
 # Base type : settings pair
 items = {
-	"01 Quality Gem 20": {"class": "Gems", "other": ["Quality >= 20"], "type": "currency high"},
+	"01 Quality Gem 20": {"class": "Gems", "other": ["Quality >= 20"], "type": "currency normal"},
 	"02 Quality Gem High": {"class": "Gems", "other": ["Quality >= 10"], "type": "gem normal"},
 	"03 Quality Gem": {"class": "Gems", "other": ["Quality >= 1"], "type": "ignore"},
 	"04 Leveled Gem ": {"class": "Gems", "other": ["GemLevel >= 2"], "type": "gem low"},
